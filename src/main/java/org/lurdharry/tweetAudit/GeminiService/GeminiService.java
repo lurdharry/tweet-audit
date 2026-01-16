a5809af46810815704ea5a833757f89eb7c3d025
lurdharry <azeezhammed57@gmail.com>
2026-01-16 12:03:57 +0100

    chore: add gemini service

---
+++ src/main/java/org/lurdharry/tweetAudit/GeminiService/GeminiService.java
@@ -0,0 +1,14 @@
+package org.lurdharry.tweetAudit.GeminiService;
+
+public class GeminiService {
+
+    private final String apiKey;
+    private final String modelName;
+
+    public GeminiService(String apiKey,String modelName) {
+        this.apiKey = apiKey;
+        this.modelName= modelName;
+    }
+
+
+}
